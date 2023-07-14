1cc2910e0b9714837daeb955a470f5dcf0ea8bef
Matthew Fogarty <mattempyre@gmail.com>
2023-07-14 14:46:17 +0200

    change cloundinary preset

--- components/ui/image-upload.tsx
+++ components/ui/image-upload.tsx
@@ -56,7 +56,7 @@ const ImageUpload: React.FC<ImageUploadProps> = ({
           </div>
         ))}
       </div>
-      <CldUploadWidget onUpload={onUpload} uploadPreset="t4drjppf">
+      <CldUploadWidget onUpload={onUpload} uploadPreset="sickfits">
         {({ open }) => {
           const onClick = () => {
             open();
